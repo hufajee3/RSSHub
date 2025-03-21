3ff57f11725b77f21dee5724c1e789cf0965aa8c
Hafaj <123440807+hufajee3@users.noreply.github.com>
2025-03-22 03:15:59 +0800

    Update vercel.ts

--- api/vercel.ts
+++ api/vercel.ts
@@ -1,15 +1,15 @@
-const path = require('path');
-const moduleAlias = require('module-alias');
+const path = import('path');
+const moduleAlias = import('module-alias');
 moduleAlias.addAlias('@', path.join(__dirname, '../lib'));
 
-const { setConfig } = require('../lib/config');
+const { setConfig } = import('../lib/config');
 setConfig({
     NO_LOGFILES: true,
 });
 
-const { handle } = require('hono/vercel');
-const app = require('../lib/app');
-const logger = require('../lib/utils/logger');
+const { handle } = import('hono/vercel');
+const app = import('../lib/app');
+const logger = import('../lib/utils/logger');
 
 logger.info(`🎉 RSSHub is running! Cheers!`);
 logger.info('💖 Can you help keep this open source project alive? Please sponsor 👉 https://docs.rsshub.app/sponsor');
